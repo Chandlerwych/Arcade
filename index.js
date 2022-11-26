58aa2f7d1d26ab894c08e2b353843df81e4c096e
Chandlerwych <chandlerwych@gmail.com>
2022-11-25 18:13:50 -0700

    WORKING BOARD

---
+++ index.js
@@ -0,0 +1,9 @@
+const gameState = {
+    players: ['x', 'o'],
+    board: [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null]
+    ]
+  }
+  // WILL THIS LAYOUT FUNCTION WITH MY DIV HTML BOARD????? COME BACK
